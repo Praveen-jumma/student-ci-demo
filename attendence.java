50ffe34a71024ba397e74a1a07e48336216c20fd
praveenjumma23@gmail.com <praveenjumma23@gmail.com>
2025-12-01 00:11:24 +0530

    all files

---
+++ attendence.java
@@ -0,0 +1,8 @@
+public class attendence {
+    public static void main(String[] args) {
+    System.out.println("attendence");
+    
+
+  }
+}
+  
---
+++ marks.java
@@ -0,0 +1,8 @@
+public class marks {
+    public static void main(String[] args) {
+    System.out.println("marks");
+   
+
+  }
+}
+
