c54ca37a5396f0665d6bae167cc783837b4d7581
praveenjumma23@gmail.com <praveenjumma23@gmail.com>
2025-11-30 23:48:13 +0530

    hello world in java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("Hello from Pipeline CI!");
+    System.out.println("Hello rahul!");
+    System.out.println("Hello karthik");
+  
+    
+    System.out.println("changes in the file");
+
+  }
+}
